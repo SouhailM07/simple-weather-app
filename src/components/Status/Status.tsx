c71e015162118c8ffb7eec91709e437998ae67e4
shadow <lightshadow416@gmail.com>
2023-09-12 16:47:40 +0100

    [Status] not closer look

--- src/components/Status/Status.tsx
+++ src/components/Status/Status.tsx
@@ -1,4 +1,23 @@
 import "./status.css";
+// assets
+// import clear from "../../assets/";
 export default function Status() {
-  return <>Status</>;
+  return (
+    <>
+      <div className="border-2 border-red-500">
+        <div className="border-2 border-black">
+          {/* <img src={clear} alt="img" className="h-[10rem] w-[10rem] " /> */}
+        </div>
+        {/*  */}
+        <div>
+          <div>c</div>
+        </div>
+        {/*  */}
+        <div>
+          <div>humidity</div>
+          <div>wind speed</div>
+        </div>
+      </div>
+    </>
+  );
 }
